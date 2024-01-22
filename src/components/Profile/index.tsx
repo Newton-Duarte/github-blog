82978eb9c9f7c2f40960a18cf11691d7f7b9daaa
Newton Duarte <newton_@hotmail.com>
2024-01-22 12:11:51 -0300

    feat: add profile component

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,40 @@
+import githubImg from '../../assets/github.svg'
+import companyImg from '../../assets/company.svg'
+import followersImg from '../../assets/followers.svg'
+import linkImg from '../../assets/link.svg'
+
+import * as S from './styles'
+import { Link } from '../Link'
+
+export function Profile() {
+  return (
+    <S.Container>
+      <S.Avatar src="https://github.com/newton-duarte.png" />
+      <S.Content>
+        <S.Header>
+          <h1>Newton Duarte</h1>
+          <Link>
+            Github
+            <img src={linkImg} />
+          </Link>
+        </S.Header>
+        <S.Description>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </S.Description>
+        <S.Information>
+          <div>
+            <img src={githubImg} /> newton_duarte
+          </div>
+          <div>
+            <img src={companyImg} /> Rocketseat
+          </div>
+          <div>
+            <img src={followersImg} /> 32 seguidores
+          </div>
+        </S.Information>
+      </S.Content>
+    </S.Container>
+  )
+}
---
+++ src/components/Profile/styles.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 864px;
+  margin: 0 auto;
+  background-color: ${(props) => props.theme.profile};
+  border-radius: 10px;
+  padding: 2rem 2.5rem;
+  margin-top: -6rem;
+  display: flex;
+  gap: 2rem;
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+export const Avatar = styled.img`
+  width: 148px;
+  height: 148px;
+  border-radius: 8px;
+`
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    color: ${(props) => props.theme.title};
+    font-size: 1.5rem;
+  }
+`
+
+export const Description = styled.p`
+  color: ${(props) => props.theme.text};
+`
+
+export const Information = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+  gap: 1.5rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`
