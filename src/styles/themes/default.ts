e62a2f3e2223e992febbeba5c8fcddac19d8b611
Newton Duarte <newton_@hotmail.com>
2024-01-22 11:39:21 -0300

    fix: font family and theme colors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,14 @@
 export const defaultTheme = {
   white: '#fff',
   bg: '#071422',
-
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
-
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
-
-  'red-300': '#F75A68',
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
+  blue: '#3294F8',
+  title: '#E7EDF4',
+  subtitle: '#C4D4E3',
+  text: '#AFC2D4',
+  span: '#7B96B2',
+  label: '#3A536B',
+  border: '#1C2F41',
+  post: '#112131',
+  profile: '#0B1B2B',
+  input: '#040F1A',
 } as const
