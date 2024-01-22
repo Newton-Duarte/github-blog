fb51769389a5e1202c3e83578ac5146134d159ec
Newton Duarte <newton_@hotmail.com>
2024-01-22 15:17:18 -0300

    feat: add main and posts component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header'
 import { Profile } from './components/Profile'
+import { Posts } from './components/Posts'
+import Main from './components/Main'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Header />
-      <Profile />
+      <Main>
+        <Profile />
+        <Posts />
+      </Main>
     </ThemeProvider>
   )
 }
