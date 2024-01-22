e3a6e4f2d89b440ada4257c714bcac5d3fff5a73
Newton Duarte <newton_@hotmail.com>
2024-01-22 15:16:38 -0300

    feat: add main component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'react'
+import * as S from './styles'
+
+export default function Main({ children }: PropsWithChildren) {
+  return <S.Main>{children}</S.Main>
+}
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+  width: 100%;
+  max-width: 960px;
+  margin: 4rem auto;
+  padding: 0 1.5rem;
+`
