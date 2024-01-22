53bf6400cac3ced3eac2ff1dca471c1426f77bc1
Newton Duarte <newton_@hotmail.com>
2024-01-22 20:17:13 -0300

    feat: implement posts component

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,38 +1,39 @@
+import { NavLink } from 'react-router-dom'
 import { Card } from '../Card'
 import { Input } from '../Input'
 import * as S from './styles'
+import { GithubIssue } from '../../hooks/useFetchGithubIssues'
 
-export function Posts() {
+type PostsProps = {
+  posts: GithubIssue[]
+}
+
+export function Posts({ posts }: PostsProps) {
   return (
     <S.Container>
       <S.SearchForm>
         <div>
           <h2>Publicações</h2>
-          <span>6 publicações</span>
+          <span>{posts?.length} publicações</span>
         </div>
         <form>
           <Input placeholder="Buscar conteúdo" />
         </form>
       </S.SearchForm>
       <S.Posts>
-        <Card
-          title="JavaScript data types and data structures"
-          date="Há 1 dia"
-          description="Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in "
-          onClick={() => console.log('clicked')}
-        />
-        <Card
-          title="JavaScript data types and data structures"
-          date="Há 1 dia"
-          description="Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in "
-          onClick={() => console.log('clicked')}
-        />
-        <Card
-          title="JavaScript data types and data structures"
-          date="Há 1 dia"
-          description="Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in "
-          onClick={() => console.log('clicked')}
-        />
+        {posts?.map((post) => (
+          <NavLink
+            key={post.id}
+            to={`/post/${post.html_url}`}
+            title={post.title}
+          >
+            <Card
+              title={post.title}
+              date={post.updated_at}
+              description={post.body}
+            />
+          </NavLink>
+        ))}
       </S.Posts>
     </S.Container>
   )
