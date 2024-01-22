a708c46a57fd50b0f7ba9838cc0f5738c4eaedae
Newton Duarte <newton_@hotmail.com>
2024-01-22 11:39:32 -0300

    style: add gradient to header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,5 +5,9 @@ export const Container = styled.header`
   align-items: center;
   justify-content: space-between;
   background-color: ${(props) => props.theme.bg};
+  background-image: linear-gradient(
+    rgba(21, 98, 175, 0.01),
+    rgba(21, 98, 175, 0.14)
+  );
   height: 296px;
 `
