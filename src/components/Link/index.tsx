0d4d29dc317f67430a8559fb1f2ad0d1dae6dc49
Newton Duarte <newton_@hotmail.com>
2024-01-22 20:16:58 -0300

    feat: implement react router dom link component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,7 +1,12 @@
 import { PropsWithChildren } from 'react'
 
 import * as S from './styles'
+import { LinkProps, NavLink } from 'react-router-dom'
 
-export function Link({ children }: PropsWithChildren) {
-  return <S.Link href="">{children}</S.Link>
+export function Link({ to, children }: PropsWithChildren & LinkProps) {
+  return (
+    <NavLink to={to}>
+      <S.Link href="">{children}</S.Link>
+    </NavLink>
+  )
 }
