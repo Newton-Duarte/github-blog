fad6fadabd2b6e9e99311a7e4a814975fd4bc149
Newton Duarte <newton_@hotmail.com>
2024-01-22 20:16:15 -0300

    feat: add react router dom config

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Header } from './components/Header'
-import { Profile } from './components/Profile'
-import { Posts } from './components/Posts'
 import Main from './components/Main'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Header />
-      <Main>
-        <Profile />
-        <Posts />
-      </Main>
+      <BrowserRouter>
+        <Header />
+        <Main>
+          <Router />
+        </Main>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
