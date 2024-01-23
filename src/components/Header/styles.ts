c91ccc58d2b71bae4a904c2a48a31ad952d431d6
Newton Duarte <newton_@hotmail.com>
2024-01-23 17:47:57 -0300

    fix: header component by adding images to the background

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,21 @@
 import styled from 'styled-components'
 
+import effect1 from '../../assets/effect1.svg'
+import effect2 from '../../assets/effect2.svg'
+
 export const Container = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  background-color: ${(props) => props.theme.bg};
-  background-image: linear-gradient(
-    rgba(21, 98, 175, 0.01),
-    rgba(21, 98, 175, 0.14)
-  );
+  justify-content: center;
+  background-image: url(${effect1}), url(${effect2});
+  background-position: left, right;
+  background-repeat: no-repeat, no-repeat;
   height: 296px;
+  position: relative;
+`
+
+export const Overlay = styled.div`
+  position: absolute;
+  inset: 0;
+  background: linear-gradient(rgba(21, 98, 175, 0.14), rgba(21, 98, 175, 0.34));
 `
