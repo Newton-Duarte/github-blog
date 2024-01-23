7f393d03d98f6db6e0835fed930ff51258c47d61
Newton Duarte <newton_@hotmail.com>
2024-01-23 16:24:56 -0300

    fix: link component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,12 +1,10 @@
 import { PropsWithChildren } from 'react'
-
-import * as S from './styles'
 import { LinkProps, NavLink } from 'react-router-dom'
 
 export function Link({ to, children }: PropsWithChildren & LinkProps) {
   return (
-    <NavLink to={to}>
-      <S.Link href="">{children}</S.Link>
+    <NavLink to={to} className="link">
+      {children}
     </NavLink>
   )
 }
